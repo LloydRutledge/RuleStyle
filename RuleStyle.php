c7784e20d7386c944e60481ce874cc4908527cfa
Rutledge <lru@ou.nl>
2021-03-29 19:59:49 +0200

    manual code tidying

--- RuleStyle.php
+++ RuleStyle.php
@@ -1,5 +1,7 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE); // turns off notice displays
+error_reporting(E_ALL ^ E_NOTICE); // turns off notice displays: FIX turn on to check
+
+/* all constants */
 
 define("EndpointURLserver", "http://localhost:3030/Fresnel/");
 define("FreselServerURLprefix", "http://localhost/RuleStyle/FresnelRules.php?resource=");
@@ -9,83 +11,81 @@ define("EndpointURLdecl", EndpointURLserver . "query?output=json&query=" . urlen
 
 define("EndpointURLstart", EndpointURLdecl . urlencode("SELECT * WHERE { "));
 
-function bindings($array)
+/* all functions */
+
+function bindings($array) // SPARQL query bindings
 {
     return $array['results']['bindings'];
 }
 
-function emptyRtn($array)
+function emptyRtn($array) // is the array empty?
 {
     return empty(bindings($array));
 }
 
-function fragment($URL)
+function fragment($URL) // fragment ID from a URI
 {
     return substr($URL, strpos($URL, "#") + 1);
 }
 
-function qryRtnCell($array, $key, $varName)
+function qryRtnCell($array, $key, $varName) // returns value assigned to variable in SPARQL return
 {
     return bindings($array)[$key][$varName]['value'];
 }
 
-function getSPARQLrtn($WHEREcontent)
+function getSPARQLrtn($WHEREcontent) // sends SPARQL query and returns results
 {
     $qryRtn = file_get_contents(EndpointURLstart . urlencode($WHEREcontent . " } "));
     return json_decode($qryRtn, true);
 }
 
-function internalLink($URL, $anchor)
+function internalLink($URL, $anchor) // an HTML link to the Fresnel browser display for another resource
 {
     print_r("<a href='" . FreselServerURLprefix . urlencode($URL) . "'>" . $anchor . "</a>\n");
 }
 
-$qryRtnAry = getSPARQLrtn(" <" . Resource . "> ?predicate ?object ");
+/* find lens for the resource as lens with query that returns resource */
 
-$qryRtnTyp = getSPARQLrtn(" ?resource a reas:Inference . FILTER ( ?resource = <" . Resource . "> ) ");
-
-$qryRtnGiv1 = getSPARQLrtn(" ?explanation reas:gives/rdf:first <" . Resource . "> ");
-
-$qryRtnGiv = getSPARQLrtn(" ?Inferred a reas:Inference ; reas:gives/rdf:rest*/rdf:first ?statement . ");
-
-$qryRtnExp = getSPARQLrtn(" ?Inferred a reas:Inference ; reas:evidence/rdf:first/rdf:rest*/rdf:first ?statement . ");
-
-$lensQueries = getSPARQLrtn(" ?lens fresnel:instanceLensDomain ?query . ");
-
-$lens = qryRtnCell(getSPARQLrtn(" ?lens fresnel:classLensDomain ?type . <" . Resource . "> rdf:type ?type . "), 0, 'lens');
-
-$propList = bindings(getSPARQLrtn(" <" . $lens . "> fresnel:showProperties/rdf:rest*/rdf:first ?prop "));
+$lens = qryRtnCell(getSPARQLrtn(" ?lens fresnel:classLensDomain ?type . <" . Resource . "> rdf:type ?type . "), 0, 'lens'); // lens from class domain
+$lensQueries = getSPARQLrtn(" ?lens fresnel:instanceLensDomain ?query . "); // all lenses with an instance query and their queries
+foreach (array_keys(bindings($lensQueries)) as $key) { // for each lens and its query
+    $thisLens = qryRtnCell($lensQueries, $key, 'lens'); // URL for this lens
+    $QResult = json_decode(file_get_contents(EndpointURLdecl . urlencode(qryRtnCell($lensQueries, $key, 'query'))), true);
+    $triggerURI = qryRtnCell($QResult, 0, 'inference'); // URL lens query returns as trigger: FIX checks any variable
+    if ($triggerURI == Resource) // this lens's query returns resource: FIX tentatative because only checks first returned
+        $lens = $thisLens; // this is the lens to apply for this resource
+}
 
 ?>
 <html>
 <head>
-<title>RuleStyle <?php print_r ( fragment ( Resource ) ) ; ?> </title>
+<title>RuleStyle <?php print_r ( fragment ( Resource ) ) ;  // browser tab label has tool and resource fragment ID ?> </title>
 </head>
 <body>
   <?php
-foreach (array_keys(bindings($lensQueries)) as $key) {
-    $thisLens = qryRtnCell($lensQueries, $key, 'lens');
-    $QResult = json_decode(file_get_contents(EndpointURLdecl . urlencode(qryRtnCell($lensQueries, $key, 'query'))), true);
-    $triggerURI = qryRtnCell($QResult, 0, 'inference');
-    if ($triggerURI == Resource)
-        $lens = $thisLens;
-}
 if ($lens == "http://example.org/#explBox") {
     print_r("<p>Explanation for inference: ");
-    foreach (array_keys(bindings($qryRtnGiv)) as $key)
+    $qryRtnGiv = getSPARQLrtn(" ?Inferred a reas:Inference ; reas:gives/rdf:rest*/rdf:first ?statement . "); // the inferred triple
+    foreach (array_keys(bindings($qryRtnGiv)) as $key) // print the triple
         print_r(fragment(qryRtnCell($qryRtnGiv, $key, 'statement')) . " ");
-    print_r("</p>\n<p>");
-    foreach (array_keys(bindings($qryRtnExp)) as $key)
+    print_r("</p>\n<p>"); // between triple and explanation
+    $qryRtnExp = getSPARQLrtn(" ?Inferred a reas:Inference ; reas:evidence/rdf:first/rdf:rest*/rdf:first ?statement . "); // an inference's explanation
+    foreach (array_keys(bindings($qryRtnExp)) as $key) // dipslay each statement
         print_r(fragment(qryRtnCell($qryRtnExp, $key, 'statement')) . " ");
-    print_r("</p>\n");
+    print_r("</p>\n"); // end of explanation
 } else {
     ?>
-	<p> <?php print_r ( fragment ( Resource ) ) ; ?> </p>
+	<p> <?php print_r ( fragment ( Resource ) ) ;  // show resource fragment?> </p>
 	<table class='resourceBox'>
         <?php
-    foreach (array_keys($propList) as $key) {
-        $predicate1 = $propList[$key]['prop']['value'];
-        print_r("<tr class=propertyBox>\n<td class='labelBox'>" . fragment($predicate1) . "</td>\n<td class='objectBox'>\n<span class='valueBox' ");
+    $qryRtnGiv1 = getSPARQLrtn(" ?explanation reas:gives/rdf:first <" . Resource . "> ");
+    $showPropList = bindings(getSPARQLrtn(" <" . $lens . "> fresnel:showProperties/rdf:rest*/rdf:first ?prop ")); // showProperties's order list of properties
+    foreach (array_keys($showPropList) as $key) {
+        $predicate1 = $showPropList[$key]['prop']['value'];
+        print_r("<tr class=propertyBox>\n"); // Fresnel box model property box
+        print_r("<td class='labelBox'>" . fragment($predicate1) . "</td>\n"); // Fresnel box model label box
+        print_r("<td class='objectBox'>\n"); // Fresnel box model object box
+        print_r("<span class='valueBox' "); // Fresnel box model value box
         if (! emptyRtn($qryRtnGiv1))
             print_r(" style='background-color:yellow' ");
         print_r(">\n");
